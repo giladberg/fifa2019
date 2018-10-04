444d584975283fc24189c8b0d48657fadf622ca4
gilad bergmann <giladberg@gmail.com>
2018-10-04 22:54:40 +0300

    4.10.18

--- fifa19/src/app/app.module.ts
+++ fifa19/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
